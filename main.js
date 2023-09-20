ea6590ded045b0b5acd53d6bf4c9c6cf6c6b3459
Astborg <93790636+Astborg@users.noreply.github.com>
2023-09-20 09:31:08 +0200

    format

--- main.js
+++ main.js
@@ -1,190 +1,166 @@
 //Hämta data från firebase rendera i DOM
 
-async function products(){
-  try{
-    const baseURL = 'https://paronab-79d88-default-rtdb.europe-west1.firebasedatabase.app/'
-    const url = baseURL + 'product.json'
-    const response = await fetch(url)
-    let data = await response.json()
-    console.log(data)
-    let date = new Date()
-    const year = date.getFullYear()
-    const month = date.getMonth()
-    const day = date.getDate()
-    
-    
-    localStorage.setItem("getdata", JSON.stringify(data))
-    
-    const lagretidag = document.querySelector('.lagretidag')
-    Object.entries(data).forEach(([key, value]) => 
-        lagretidag.innerHTML += `
+async function products() {
+  try {
+    const baseURL =
+      "https://paronab-79d88-default-rtdb.europe-west1.firebasedatabase.app/";
+    const url = baseURL + "product.json";
+    const response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
+    let date = new Date();
+    const year = date.getFullYear();
+    const month = date.getMonth();
+    const day = date.getDate();
+
+    localStorage.setItem("getdata", JSON.stringify(data));
+
+    const lagretidag = document.querySelector(".lagretidag");
+    Object.entries(data).forEach(
+      ([key, value]) =>
+        (lagretidag.innerHTML += `
         <div class="produkt-container">
-        <p>${year}-0${month+1}-${day}</p>
+        <p>${year}-0${month + 1}-${day}</p>
         <p class="rubrik">Produkt: ${key}</p>
         <p>Frankfurt: ${value.Frankfurt}</p>
         <p>Cupertino: ${value.Cupertino}</p> 
         <p>Norrköping: ${value.Norrköping}</p>
         </div>
         <br>
-        `
-        
-    )
-
-   
-  }catch(error){
-    console.log(error)
+        `)
+    );
+  } catch (error) {
+    console.log(error);
   }
-
-  }
-products()
-
+}
+products();
 
 //Hämta värden från ifyllda fält och spara i localStorage
-const euro = document.querySelectorAll('.euro')
-function change1(){
-    const input1 = document.querySelector('.input1').value
-    const input2 =  document.querySelector('.input2').value
-    const select = document.getElementById('select').value
-    const select2 = document.getElementById('select2').value
-    
-    console.log(input1, input2, select)
+const euro = document.querySelectorAll(".euro");
+function change1() {
+  const input1 = document.querySelector(".input1").value;
+  const input2 = document.querySelector(".input2").value;
+  const select = document.getElementById("select").value;
+  const select2 = document.getElementById("select2").value;
 
-    const arr = new Array();
-    arr.push({input1, input2, select, select2});
+  console.log(input1, input2, select);
 
-    localStorage.setItem("myArray", JSON.stringify(arr));  
-    localStorage.setItem('select2', select2)
-    localStorage.setItem('select', select)
-    localStorage.setItem('input2', input2)
-    localStorage.setItem('input1', input1)
+  const arr = new Array();
+  arr.push({ input1, input2, select, select2 });
+
+  localStorage.setItem("myArray", JSON.stringify(arr));
+  localStorage.setItem("select2", select2);
+  localStorage.setItem("select", select);
+  localStorage.setItem("input2", input2);
+  localStorage.setItem("input1", input1);
 }
-  
 
 // todo code
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const todosUL = document.getElementById('todos')
-const todo2 = document.querySelector('.todo2')
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+const todo2 = document.querySelector(".todo2");
 
-const todos = JSON.parse(localStorage.getItem('todos'))
-if(todos){
-    todos.forEach(todo => addTodo(todo))
+const todos = JSON.parse(localStorage.getItem("todos"));
+if (todos) {
+  todos.forEach((todo) => addTodo(todo));
 }
 
-async function renderTodo(){
-  try{
-    const baseURL = 'https://todoparon-default-rtdb.europe-west1.firebasedatabase.app/'
-    const url = baseURL + 'todo.json'
-    const response = await fetch(url)
-    let data = await response.json()
-    console.log(data)
-    
+async function renderTodo() {
+  try {
+    const baseURL =
+      "https://todoparon-default-rtdb.europe-west1.firebasedatabase.app/";
+    const url = baseURL + "todo.json";
+    const response = await fetch(url);
+    let data = await response.json();
+    console.log(data);
 
-    Object.entries(data).forEach(([key, value]) => 
-      todo2.innerHTML += `
-      <p>${value}</p>` 
-    )
-  }catch(error){
-    console.log(error)
+    Object.entries(data).forEach(
+      ([key, value]) =>
+        (todo2.innerHTML += `
+      <p>${value}</p>`)
+    );
+  } catch (error) {
+    console.log(error);
   }
-
-  }
-renderTodo()
+}
+renderTodo();
 async function addTodoToFirebase() {
   try {
-    const input = document.getElementById('input').value
-    
-    
-  
-    const baseURL = 'https://todoparon-default-rtdb.europe-west1.firebasedatabase.app/'
-    const url = baseURL + 'todo.json'
-    const response = await fetch(url)
-    console.log(response)
-    let data = await response.json()
-    console.log(data)
-   console.log(input)
-   
-    
+    const input = document.getElementById("input").value;
+
+    const baseURL =
+      "https://todoparon-default-rtdb.europe-west1.firebasedatabase.app/";
+    const url = baseURL + "todo.json";
+    const response = await fetch(url);
+    console.log(response);
+    let data = await response.json();
+    console.log(data);
+    console.log(input);
 
     // Send the updated data back to Firebase
     const updateResponse = await fetch(url, {
-      method: 'POST', 
+      method: "POST",
       body: JSON.stringify(input),
       headers: {
-        'Content-type': 'application/json; charset=UTF-8',
+        "Content-type": "application/json; charset=UTF-8",
       },
     });
 
     if (updateResponse.ok) {
-      console.log('Data updated successfully in Firebase!');
+      console.log("Data updated successfully in Firebase!");
     } else {
-      console.error('Failed to update data in Firebase.');
+      console.error("Failed to update data in Firebase.");
     }
   } catch (error) {
-    console.error('Error while updating data:', error);
+    console.error("Error while updating data:", error);
   }
 }
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    addTodoToFirebase()
-    addTodo() 
-    
-    
-})
-function addTodo(todo){
-    let todoText = input.value 
-    console.log(todoText)
-    if(todo){
-    
-        
+  addTodoToFirebase();
+  addTodo();
+});
+function addTodo(todo) {
+  let todoText = input.value;
+  console.log(todoText);
+  if (todo) {
+  }
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
     }
-    if(todoText){
-        const todoEl = document.createElement('li')
-        if(todo && todo.completed){
-            todoEl.classList.add('completed')
-        }
-        todoEl.innerText = todoText
+    todoEl.innerText = todoText;
 
-        todoEl.addEventListener('click', () =>{
-            todoEl.classList.toggle('completed')
-        })
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+    });
 
-        todoEl.addEventListener('contextmenu', (e) => {
-            e.preventDefault()
-            todoEl.remove()
-            updateLS()
-        })
+    todoEl.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+      todoEl.remove();
+      updateLS();
+    });
 
-        todosUL.appendChild(todoEl)
+    todosUL.appendChild(todoEl);
 
-        input.value = ''
+    input.value = "";
 
-        updateLS()
-       
-    }
+    updateLS();
+  }
 }
-function updateLS(){
-    const todosEl = document.querySelectorAll('li')
+function updateLS() {
+  const todosEl = document.querySelectorAll("li");
 
-    const todos = []
-    todosEl.forEach(todoEl => {
-        todos.push({
-            text: todoEl.innerText, 
-            completed: todoEl.classList.contains('completed')
-        })
-    })
-    localStorage.setItem('todos', JSON.stringify(todos))
-    
+  const todos = [];
+  todosEl.forEach((todoEl) => {
+    todos.push({
+      text: todoEl.innerText,
+      completed: todoEl.classList.contains("completed"),
+    });
+  });
+  localStorage.setItem("todos", JSON.stringify(todos));
 }
-
-
-
-
-
-
-
-    
-         
-
--- modules/cart.js
+++ modules/cart.js
@@ -1,86 +1,74 @@
 //Hämta data från localStorage rendera ut produkten som ska ändras i lagret
-let productGrid = document.querySelector('.grid')
+let productGrid = document.querySelector(".grid");
 
+let str = localStorage.getItem("myArray");
+arr = JSON.parse(str);
+console.log(arr);
 
-   
-      let str = localStorage.getItem('myArray')
-      arr = JSON.parse(str)
-      console.log(arr)
-  
-  
-
-    Object.entries(arr).forEach( ([key, value]) =>
-      productGrid.innerHTML += `
+Object.entries(arr).forEach(
+  ([key, value]) =>
+    (productGrid.innerHTML += `
       <div class="product">Produkt: ${value.select2}</div>
       <div class="quantity">Antal ta bort: -${value.input1}</div>
       <div class="quantity">Antal lägga till: +${value.input2}</div>
-      <div>Stad: ${value.select}</div>`
-    )
-    
-    
-  
-  //knappar med tömningsfunktion och gör ändringar i lagret
-    productGrid.innerHTML += `<button class="btn1" onclick="emptyCart()">Töm ändringar</button>`
-    productGrid.innerHTML += `<button class="btn2" onclick="checkout(id)">Ändra i lagret</button>`
+      <div>Stad: ${value.select}</div>`)
+);
 
-  
+//knappar med tömningsfunktion och gör ändringar i lagret
+productGrid.innerHTML += `<button class="btn1" onclick="emptyCart()">Töm ändringar</button>`;
+productGrid.innerHTML += `<button class="btn2" onclick="checkout(id)">Ändra i lagret</button>`;
 
-// Tömma ändringar 
-function emptyCart(){
-  let productGrid = document.querySelector('.grid')
-  productGrid.innerHTML = ''
+// Tömma ändringar
+function emptyCart() {
+  let productGrid = document.querySelector(".grid");
+  productGrid.innerHTML = "";
   localStorage.clear();
 }
 
-
-//hämta data från Firebase från localStorage 
-const getData = localStorage.getItem('getdata')
-const parseData = JSON.parse(getData)
-console.log(parseData)
-const select2 = localStorage.getItem('select2')
-const select = localStorage.getItem('select')
-const input2 = localStorage.getItem('input2')
-const newInput2 = JSON.parse(input2)
-const input1 = localStorage.getItem('input1')
-const newInput1 = JSON.parse(input1)
-
+//hämta data från Firebase från localStorage
+const getData = localStorage.getItem("getdata");
+const parseData = JSON.parse(getData);
+console.log(parseData);
+const select2 = localStorage.getItem("select2");
+const select = localStorage.getItem("select");
+const input2 = localStorage.getItem("input2");
+const newInput2 = JSON.parse(input2);
+const input1 = localStorage.getItem("input1");
+const newInput1 = JSON.parse(input1);
 
 //Skicka ändringar till Firebase
-function checkout(){
+function checkout() {
+  async function updateSelectValue() {
+    try {
+      const baseURL =
+        "https://paronab-79d88-default-rtdb.europe-west1.firebasedatabase.app/";
+      const url = baseURL + `product/${select2}/${select}.json`;
+      const response = await fetch(url);
+      let data = await response.json();
+      console.log(data);
+      data += newInput2;
+      data -= newInput1;
 
-            
-              
-              async function updateSelectValue() {
-                try {
-                  const baseURL = 'https://paronab-79d88-default-rtdb.europe-west1.firebasedatabase.app/'
-                  const url = baseURL + `product/${select2}/${select}.json`
-                  const response = await fetch(url)
-                  let data = await response.json()
-                  console.log(data)
-                  data += newInput2
-                  data -= newInput1
-  
-                  // Send the updated data back to Firebase
-                  const updateResponse = await fetch(url, {
-                    method: 'PUT', // Use PUT to update the entire data
-                    body: data,
-                    headers: {
-                      'Content-type': 'application/json; charset=UTF-8',
-                    },
-                  });
-              
-                  if (updateResponse.ok) {
-                    console.log('Data updated successfully in Firebase!');
-                  } else {
-                    console.error('Failed to update data in Firebase.');
-                  }
-                } catch (error) {
-                  console.error('Error while updating data:', error);
-                }
-              }
-              
-              // Usage example: Call the updateSelectValue function to update 'select' based on 'select2'
-              updateSelectValue('select2', 'select');
-            emptyCart()
-        }
-              
+      // Send the updated data back to Firebase
+      const updateResponse = await fetch(url, {
+        method: "PUT", // Use PUT to update the entire data
+        body: data,
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      });
+
+      if (updateResponse.ok) {
+        console.log("Data updated successfully in Firebase!");
+      } else {
+        console.error("Failed to update data in Firebase.");
+      }
+    } catch (error) {
+      console.error("Error while updating data:", error);
+    }
+  }
+
+  // Usage example: Call the updateSelectValue function to update 'select' based on 'select2'
+  updateSelectValue("select2", "select");
+  emptyCart();
+}
